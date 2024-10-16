5a9903ee4460189475927c4da46d8f68d34c460b
Pedroca <132005678+Pedro3DS@users.noreply.github.com>
2024-10-16 19:22:01 -0400

    Update

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -7,6 +8,10 @@ public class Player : MonoBehaviour
 
     private Rigidbody2D _rb2d;
     [SerializeField] private GameObject shoot;
+    public float shootSpeed;
+
+    [SerializeField] private float shootCadence = 0.5f;
+    private float _nextShoot = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -24,15 +29,43 @@ public class Player : MonoBehaviour
     void Movement(){
         float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
-        Debug.Log(horizontal);
-        _rb2d.velocity = new Vector2(horizontal, vertical ) * 10f;
+
+        _rb2d.velocity = new Vector2(horizontal, vertical ) * 5f;
         
     }
 
     void Shoot(){
-        if(Input.GetKeyDown(KeyCode.Alpha1)){
-            Debug.Log("ghjkl");
-            GameObject newShoot =  Instantiate(shoot, gameObject.transform);
+        if (Time.time >= _nextShoot && (Input.GetKeyDown(KeyCode.Keypad1) || Input.GetKeyDown(KeyCode.Keypad4) || Input.GetKeyDown(KeyCode.Keypad2) || Input.GetKeyDown(KeyCode.Keypad5)))
+        {
+
+            _nextShoot = Time.time + shootCadence;
+
+            Vector2 direction = Vector2.left;
+            // if(Input.GetKeyDown(KeyCode.Keypad1)){
+            //     direction = Vector2.left;
+            // }
+            if(Input.GetKeyDown(KeyCode.Keypad2)){
+                direction = Vector2.right;
+            }
+            else if(Input.GetKeyDown(KeyCode.Keypad4)){
+                direction = Vector2.up;
+            }
+            else if(Input.GetKeyDown(KeyCode.Keypad5)){
+                direction = Vector2.down;
+            }
+
+            
+
+            Vector3 spawnPosition = transform.position + new Vector3(direction.x, direction.y, 0f);
+
+
+            GameObject newBullet = Instantiate(shoot, spawnPosition, Quaternion.identity);
+
+
+            Rigidbody2D bulletRB = newBullet.GetComponent<Rigidbody2D>();
+            bulletRB.velocity = direction * shootSpeed;
+
+            Destroy(bulletRB, 2);
         }
         
     }
